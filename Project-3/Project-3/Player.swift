e662962ce2ef3fb446a2f87a907ca6c103909522
Kylta <Christophe.bugnon02@gmail.com>
2017-11-10 14:24:24 +0100

    Add method createTeam()  in class Player for user choose his characters

--- Project-3/Project-3/Player.swift
+++ Project-3/Project-3/Player.swift
@@ -21,4 +21,39 @@ class Player {
     init(name: String) {
         self.name = name
     }
+    
+    // Method for player create his team
+   func createTeam() {
+        
+        // repeat while array team contains 3 characters
+        while team.count < 3 {
+            print("\nMake a choice !")
+            // constante for keep the choice of user in Tools.answerInt()
+            let userChoice = Tools.answerInt()
+            // Use it with while => Comments below near while
+            repeat {
+                // We switch userChoice, if user make choice 1 he will pick warrior, 2: giant etc...
+                switch userChoice {
+                case 1:
+                    print("You choose Warrior")
+                    let warrior = Warrior()
+                    team.append(warrior)
+                case 2:
+                    print("You choose Giant")
+                    let giant = Giant()
+                    team.append(giant)
+                case 3:
+                    print("You choose Rogue")
+                    let rogue = Rogue()
+                    team.append(rogue)
+                case 4:
+                    print("You choose Mage")
+                    let mage = Mage()
+                    team.append(mage)
+                default:
+                    print("Choose a number between 1 and 4.\nI don't understand !")
+                }
+            } while (userChoice != 1 && userChoice != 2 && userChoice != 3 && userChoice != 4) // While player isn't in the range between 1..4 it repeat
+        }
+    }
 }
